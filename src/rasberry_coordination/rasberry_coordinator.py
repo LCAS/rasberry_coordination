d4b03a7505986bc03e9260369549720236ef1976
Iranaphor <Primordia@live.com>
2021-11-17 17:08:14 +0000

    Task Progression Log location updated

--- src/rasberry_coordination/rasberry_coordinator.py
+++ src/rasberry_coordination/rasberry_coordinator.py
@@ -155,7 +155,7 @@ class RasberryCoordinator(object):
 
         A = get_agents()
         self.enable_task_logging = True
-        self.task_progression_log = '/home/jheselden/task_progression.csv'
+        self.task_progression_log = 'task_progression.csv' #logs to $HOME/.ros/task_progression.csv
         self.log_routes = True
         self.action_print = True
         self.timestep = 0
