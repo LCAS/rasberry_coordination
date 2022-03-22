f682b435dcd66c583fa8caed0ca4d7da41f7321a
Iranaphor <Primordia@live.com>
2022-03-22 13:34:42 +0100

    New Tmule formatting begun

---
+++ new_tmule/shell/mongo.sh
@@ -0,0 +1,6 @@
+# path where to expect MONGODB
+export_default MONGO_PATH "$HOME/mongodb"
+export_default ROBOTS_MONGO_PATH $MONGO_PATH/robots
+export_default ROBOTS_MONGO_PORT 62345
+export_default SERVER_MONGO_PATH $MONGO_PATH/server
+export_default SERVER_MONGO_PORT 62346
