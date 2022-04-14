4c7c1cd2ff65028acf5c43aa25404edc485879d0
Iranaphor <Primordia@live.com>
2022-04-14 13:40:38 +0100

    Inclusion of AgentConfig Monitoring Tool

--- scripts/add_agent.py
+++ scripts/add_agent.py
@@ -2,56 +2,108 @@
 
 import sys
 import rospy
+from std_msgs.msg import String
 import rasberry_coordination
 from rasberry_coordination.msg import AgentDetails, Module, KeyValuePair
 import rasberry_des.config_utils
 from rasberry_coordination.coordinator_tools import logmsg, logmsgbreak
 
 
-if __name__ == '__main__':
+def get_kvp_list(dict, item):
+    if item in dict:
+        return [KeyValuePair(k, str(v)) for k, v in dict[item].items()]
+    return []
 
-    # Initialise node
-    rospy.init_node("AddAgent", anonymous=True)
-    rospy.sleep(1)
-    logmsg(category="DRM", msg="AddAgent Node launched")
 
-    # Collect details
-    agent_file = sys.argv[1]
-    setup_file = sys.argv[2]
+import rospkg
+def get_file_path(agent, setup):
+    rc=rospkg.RosPack().get_path('rasberry_coordination')
+    agent_file = "%s/config/agent/%s.yaml"%(rc, agent)
+    setup_file = "%s/config/setup/%s.yaml"%(rc, setup)
+    return agent_file, setup_file
 
-    logmsgbreak()
-    logmsg(category="DRM", msg="Loading configurations:")
-    logmsg(category="DRM", msg="    - agent_file: %s"%agent_file)
-    logmsg(category="DRM", msg="    - setup_file: %s"%setup_file)
+
+def load_agent_obj(agent_input, setup_input, get_files_from_paths=False):
 
     # Load config file
-    agent_data = rasberry_des.config_utils.get_config_data(agent_file)
+    if get_files_from_paths:
+        agent_file, setup_file = get_file_path(agent_input, setup_input)
+    else:
+        agent_file, setup_file = agent_input, setup_input
+
+    try:
+        agent_data = rasberry_des.config_utils.get_config_data(agent_file)
+    except Exception as e:
+        print(e)
+        logmsg(level="error", category="DRM", msg="File not Loaded: %s" % (agent_file))
+        agent_data = {'agent_id': agent_input}
+        logmsg(level="error", category="DRM", msg="Launching with agent_data: %s" % (agent_data))
+
     setup_data = rasberry_des.config_utils.get_config_data(setup_file)
 
-    # Check item in dict for KVP list generation
-    def get_kvp_list(dict, item):
-        if item in dict:
-            return [KeyValuePair(k, str(v)) for k, v in dict[item].items()]
-        return []
+    if not get_files_from_paths:
+        agent_data['agent_id'] = agent_file
 
     # Build msg
     agent = AgentDetails()
     agent.agent_id = agent_data['agent_id']
     agent.local_properties = get_kvp_list(agent_data, 'local_properties')
     agent.setup.modules = [Module(m['name'], m['role']) for m in setup_data['modules']]
-    agent.setup.module_properties =        get_kvp_list(setup_data, 'module_properties')
-    agent.setup.navigation_properties =    get_kvp_list(setup_data, 'navigation_properties')
+    agent.setup.module_properties = get_kvp_list(setup_data, 'module_properties')
+    agent.setup.navigation_properties = get_kvp_list(setup_data, 'navigation_properties')
     agent.setup.visualisation_properties = get_kvp_list(setup_data, 'visualisation_properties')
+    return agent
 
-    logmsg(category="DRM", msg="Details of Agent being launched:\n%s\n\n"%agent)
 
-    # Create publisher
-    # pub = rospy.Publisher("/a", AgentDetails, latch=True, queue_size=5)
-    pub = rospy.Publisher("/rasberry_coordination/dynamic_fleet/add_agent", AgentDetails, latch=True, queue_size=5)
-    # pub = rospy.Publisher("launch_agent", AgentDetails, latch=True, queue_size=5)
+class AgentMonitor():
+    def __init__(self):
+        self.pub = rospy.Publisher("/rasberry_coordination/dynamic_fleet/add_agent", AgentDetails, latch=True, queue_size=5)
+        self.s1 = rospy.Subscriber("/car/new_agent", String, self.new_picker)
+        self.s2 = rospy.Subscriber("/sar/new_agent", String, self.new_tall_controller)
+
+    def new_picker(self, msg):
+        logmsg(category="DRM", msg="Recieved new picker information: %s"%(msg.data))
+        agent = load_agent_obj(agent_input=msg.data, setup_input='picker', get_files_from_paths=True)
+        print(agent)
+        self.pub.publish(agent)
+
+    def new_tall_controller(self, msg):
+        logmsg(category="DRM", msg="Recieved new tall_controller information: %s"%(msg.data))
+        agent = load_agent_obj(agent_input=msg.data, setup_input='tall_controller', get_files_from_paths=True)
+        print(agent)
+        self.pub.publish(agent)
+
+
+if __name__ == '__main__':
+    # Initialise node
+    rospy.init_node("AddAgent", anonymous=True)
     rospy.sleep(1)
 
-    while not rospy.is_shutdown():
-        pub.publish(agent)
-        logmsg(category="null", msg="publishing")
-        rospy.sleep(5)
+    if len(sys.argv) < 5:
+        logmsg(category="DRM", msg="AddAgent Monitor launched")
+        monitor = AgentMonitor()
+        rospy.spin()
+
+    else:
+        logmsg(category="DRM", msg="AddAgent Node launched")
+
+        # Collect details
+        agent_file = sys.argv[1]
+        setup_file = sys.argv[2]
+
+        logmsgbreak()
+        logmsg(category="DRM", msg="Loading configurations:")
+        logmsg(category="DRM", msg="    - agent_file: %s"%agent_file)
+        logmsg(category="DRM", msg="    - setup_file: %s"%setup_file)
+
+        agent = load_agent_obj(agent_file, setup_file)
+        logmsg(category="DRM", msg="Details of Agent being launched:\n%s\n\n"%agent)
+
+        # Create publisher
+        pub = rospy.Publisher("/rasberry_coordination/dynamic_fleet/add_agent", AgentDetails, latch=True, queue_size=5)
+        rospy.sleep(1)
+
+        while not rospy.is_shutdown():
+            pub.publish(agent)
+            logmsg(category="null", msg="publishing")
+            rospy.sleep(5)
