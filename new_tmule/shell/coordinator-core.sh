34495748758e4504d630b9107a1234c41d50427b
James Heselden <primordia@live.com>
2022-06-13 14:29:00 +0100

    coordinator reference updates

--- new_tmule/shell/coordinator-core.sh
+++ new_tmule/shell/coordinator-core.sh
@@ -1,2 +1,2 @@
-export_default TMAP_FILE "${SITE_PATH}/${SYSTEM_APPLICATION}/tmap.tmap2"
-export_default TASK_COORD_CONFIG "${SITE_PATH}/${AGENT_APPLICATION}/rb_atm.yaml"
+export_default TMAP_FILE "${SITE_PATH}/${MAPS}/tmap.tmap2"
+export_default TASK_COORD_CONFIG "${SITE_PATH}/server/rb_atm.yaml"
