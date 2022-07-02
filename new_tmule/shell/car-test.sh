86978e08536b83587f9b371426e27c655be24237
JHeselden <primordia@live.com>
2022-07-02 13:53:54 +0100

    rasberry_interfacing created - prep for BT FCD integration

--- new_tmule/shell/car-test.sh
+++
@@ -1,7 +0,0 @@
-#export_default WEBSOCKET_URL 'wss://lcas.lincoln.ac.uk/car/ws'  # car-lcas
-#export_default WEBSOCKET_URL 'ws://0.0.0.0:8128/rasberry/ws'  # car-local
-export_default WEBSOCKET_URL 'ws://10.8.0.18:8128/rasberry/ws'  # car-test
-
-export_default GMAPS_API "AIzaSyCniXaQ1L0MYa_T4xNwPMokJSjkDIcUcvo" #for browser location access
-
-export_default LAUNCH_CAR true
