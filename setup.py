780c0bdecd63fc72e149b334ed5c51350834d8ed
RASBerry Server <you@example.com>
2022-07-25 13:51:41 +0100

    python dependency listings

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-##    install_requires=["tmule==1.5.9"],
+    install_requires=["whiptail==0.2"],
     packages=['rasberry_coordination'],
     package_dir={'': 'src'}
 )
