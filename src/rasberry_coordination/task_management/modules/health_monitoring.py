49bf496759dcccd707d02eb515ee22728fcba444
RASBerry Server <you@example.com>
2022-08-01 09:08:14 +0100

    health_monitoring debugging

--- src/rasberry_coordination/task_management/modules/health_monitoring.py
+++ src/rasberry_coordination/task_management/modules/health_monitoring.py
@@ -1,7 +1,7 @@
 """Health Monitoring"""
 
 from copy import deepcopy
-from std_msgs.msg import String as Str, Bool
+from std_msgs.msg import String as Str, Bool, Float32
 from rospy import Time, Duration, Subscriber, Publisher, Time
 from rasberry_coordination.actions.action_manager import ActionDetails
 from rasberry_coordination.coordinator_tools import logmsg
@@ -24,8 +24,8 @@ class InterfaceDef(object):
             # self.motor_have_scripts = True
             # self.motor_issues_sub = Subscriber('/%s/health_monitoring/motor_issues' % (self.agent.agent_id), Bool, self.motor_issues_cb)
 
-            self.motors_are_turned_on = None
-            self.motor_status_sub = Subscriber('/%s/health_monitoring/motor_status' % (self.agent.agent_id), Bool, self.motor_status_cb)
+            self.motors_turned_on = False
+            self.motors_turned_on_sub = Subscriber('/%s/health_monitoring/motors_turned_on' % (self.agent.agent_id), Bool, self.motors_turned_on_cb)
 
             self.agent.local_properties['battery_level'] = None
             self.battery_estimate_sub = Subscriber('/%s/health_monitoring/battery_estimate' % (self.agent.agent_id), Float32, self.battery_estimate_cb)
@@ -73,10 +73,10 @@ class InterfaceDef(object):
         #     else:
         #         logmsg(level="warn", category="HEALTH", id=self.agent.agent_id, msg="Agent has missing scripts.")
 
-        def motor_status_cb(self, motor_status):
-            if self.motors_are_turned_on == motor_status.data: return
-            self.motors_are_turned_on = motor_status.data
-            if self.motors_are_turned_on:
+        def motors_turned_on_cb(self, motors_turned_on):
+            if self.motors_turned_on == motors_turned_on.data: return
+            self.motors_turned_on = motors_turned_on.data
+            if self.motors_turned_on:
                 logmsg(level="warn", category="HEALTH", id=self.agent.agent_id, msg="Agent motors are turned ON.")
                 self.speaker("motors: on")
                 self.enable_navigation()
@@ -88,6 +88,9 @@ class InterfaceDef(object):
         def battery_estimate_cb(self, battery_estimate):
             # Save the data to the agent object
             self.agent.local_properties['battery_level'] = battery_estimate.data
+
+            # Check if motors are turned on, if not we shouldnt monitor battery output
+            if not self.motors_turned_on: return
 
             # Add charging task if battery is critical, and agent isnt charging
             if self.battery_critical() and not self.is_charging():
