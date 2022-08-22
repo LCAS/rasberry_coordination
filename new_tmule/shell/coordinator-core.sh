212ab6fb69216670780ecd77170a0916d81ff468
Your Name <you@example.com>
2022-08-22 15:28:40 +0100

    023 working state

--- new_tmule/shell/coordinator-core.sh
+++ new_tmule/shell/coordinator-core.sh
@@ -1,2 +1,2 @@
-export_default TMAP_FILE "${SITE_PATH}/${MAPS}/tmap.tmap2"
+export_default TMAP_FILE "${SITE_PATH}/combined/tmap.tmap2"
 export_default TASK_COORD_CONFIG "${SITE_PATH}/server/rb_atm.yaml"
