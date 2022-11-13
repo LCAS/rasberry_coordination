32c8372e24078f30642919f443014f5b608903ac
rasberry-dev-pc <you@example.com>
2022-11-13 16:58:48 +0000

    tidying

--- new_tmule/shell/rviz.sh
+++ new_tmule/shell/rviz.sh
@@ -1,5 +1,5 @@
 # RViZ Config
-export_default DISPLAY ":0"
+export_default DISPLAY ":0.0"
 if ! $USE_SIM; then
   export_default RVIZ_CONFIG "server"
 else
@@ -7,10 +7,10 @@ else
 fi
 
 # Site details
-export_default METRIC_MAP "${SITE_PATH}/map.yaml"
-export_default NOGO_MAP "${SITE_PATH}/nogo_map.yaml"
+#export_default METRIC_MAP "${SITE_PATH}/map.yaml"
+#export_default NOGO_MAP "${SITE_PATH}/nogo_map.yaml"
 
 # Coordination configs
-export_default TASK_COORD_CONFIG "${SITE_PATH}/${AGENT_APPLICATION}/rb_atm.yaml"
+#export_default TASK_COORD_CONFIG "${SITE_PATH}/${AGENT_APPLICATION}/rb_atm.yaml"
 
 
