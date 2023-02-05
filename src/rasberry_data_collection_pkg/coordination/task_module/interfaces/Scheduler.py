6781783f9040bf44243de94f9825d8de3b8bf751
Iranaphor <primordia@live.com>
2023-02-05 17:30:50 +0000

    support for scheduling interactions

--- src/rasberry_data_collection_pkg/coordination/task_module/interfaces/Scheduler.py
+++ src/rasberry_data_collection_pkg/coordination/task_module/interfaces/Scheduler.py
@@ -10,13 +10,14 @@ class Scheduler(Interface):
 
     @classmethod
     def list_schedulable_tasks(cls):
-        t1 = TaskContents(task_name='scan_row', nodes=['r1-c1'])
-        t2 = TaskContents(task_name='scan_edge', nodes=['r1-cb', 'r1-c0'])
+        t1 = TaskContents(task_name='scan_row', nodes=['required | 1 only'], viable_agents=['optional'])
+        t2 = TaskContents(task_name='scan_edge', nodes=['required | 2 only'], viable_agents=['optional'])
         return [t1,t2]
 
     def assign_scan_row(self, task_id=None, details=None, contacts=None, initiator_id=""):
         logmsg(category="SCHEDU", id=self.agent.agent_id, msg="Scheduled assignment for: %s"%details['msg'].criteria.task_name)
 
+        # Perform validation
         if not details['msg'].criteria.nodes:
             logmsg(category="SCHEDU", msg="    | no row id included")
             return
@@ -34,18 +35,22 @@ class Scheduler(Interface):
                     ]))
 
     def assign_scan_edge(self, task_id=None, details=None, contacts=None, initiator_id=""):
-        #details = details or dict()
-        #details["action_time_limit"] = 100 #TODO what fallout arises from fields here moving towards start of pipeline?
-
         logmsg(category="SCHEDU", id=self.agent.agent_id, msg="Scheduled assignment for: %s"%details['msg'].criteria.task_name)
 
+        # Perform validation
         nodes = details['msg'].criteria.nodes
-        if not len(nodes) == 2:
-            logmsg(level='error', category="SCHEDU", msg="    | lacking 2 ends of edge regino included")
+        if len(nodes) < 2:
+            logmsg(level='error', category="SCHEDU", msg="    | too few nodes included")
+            return
+        if len(nodes) > 2:
+            logmsg(level='error', category="SCHEDU", msg="    | too many nodes included")
             return
         if nodes[0].split('-')[0] != nodes[1].split('-')[0]:
             logmsg(level='error', category="SCHEDU", msg="    | nodes are not of same row")
             return
+
+        #details = details or dict()
+        #details["action_time_limit"] = 100 #TODO what fallout arises from fields here moving towards start of pipeline?
 
         return(Task(id=task_id,
                     module='rasberry_data_collection_pkg',
