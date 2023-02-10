48fd06f89a311fc053bb71e1936b764cd251fc70
JamesH <primordia@live.com>
2023-02-10 17:39:48 +0000

    update config file locations

--- new_tmule/shell/coordinator-tools.sh
+++ new_tmule/shell/coordinator-tools.sh
@@ -1,2 +1,3 @@
 export_default LOGMSG_CONFIG "${DEFAULT_PATH}/server/logmsg.yaml"
 export_default AGENT_SETUP_CONFIG "${DEFAULT_PATH}/server/setup/"
+export_default RVIZ_STRUCTURES_CONFIG "${DEFAULT_PATH}/server/rviz_structures.yaml"
