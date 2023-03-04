e21ca60a3404229b715f02c7d767a42a5bc6d112
JamesH <primordia@live.com>
2023-03-04 20:05:00 +0000

    folder structure to get colcon to build python pkg

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-#!/usr/bin/env python
+#from distutils.core import setup
+#from catkin_pkg.python_setup import generate_distutils_setup
+#
+#d = generate_distutils_setup(
+#    install_requires=["whiptail==0.2"],
+#    packages=['rasberry_coordination'],
+#    package_dir={'': 'src'}
+#)
+#
+#setup(**d)
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-d = generate_distutils_setup(
-    install_requires=["whiptail==0.2"],
-    packages=['rasberry_coordination'],
-    package_dir={'': 'src'}
+package_name = 'rasberry_coordination'
+
+setup(
+    name=package_name,
+    version='1.3.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='james',
+    maintainer_email='primordia@live.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
 
-setup(**d)
