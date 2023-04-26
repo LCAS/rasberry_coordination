a5957f33e4ca1c90bf6e9ea00d1863126ee5262a
mobile_server <example@email.com>
2023-04-26 12:31:18 +0100

    typo

--- src/rasberry_coordination/agent_management/manager.py
+++ src/rasberry_coordination/agent_management/manager.py
@@ -17,7 +17,7 @@ from std_srvs.srv import Trigger, TriggerResponse
 
 from diagnostic_msgs.msg import KeyValue
 from rasberry_coordination.msg import NewAgentConfig, MarkerDetails
-from rasberry_coordination.msg import Agent, AgentList, AgentRegistration, AgentState, AgentLocation, AgentHealth, AgentRendering
+from rasberry_coordination.msg import Agent, AgentList, AgentState, AgentLocation, AgentHealth, AgentRendering
 from rasberry_coordination.coordinator_tools import logmsg
 from rasberry_coordination.agent_management.location_handler import LocationObj as Location
 from rasberry_coordination.topomap_management.map_handler import MapObj as Map
